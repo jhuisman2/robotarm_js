cb4d53429dee7e5da4eb0b49042c0264f992ce62
Jan-Willem Huisman <contact@jwhuisman.nl>
2016-07-01 20:51:59 +0200

    Deleted some files. Added functions for move left, move right and grab. Animations not working yet.

--- robotarm.js
+++ robotarm.js
@@ -12,6 +12,7 @@ var robot_arm = {
   assembly_line : {}
 }
 
+var exercise = 0;
 var max_duration = 2000;
 var station_width = 50;
 var station_count = 10;
@@ -21,8 +22,18 @@ var arm_height = 10;
 var hand_height = 10;
 var level_count = 8;
 var line_position = arm_height + level_count * block_height;
+var brush = null;
+var framerate = (1000 / 30); // 1000 ms = 1 sec. 30 frames a sec.
 
-var brush = "";
+
+
+function set_level(f_id){
+	if(f_id < 1 || f_id > 15){
+		console.log("The level has to be between 1 and 15");
+	} else {
+		exercise = f_id;
+	}
+}
 
 function draw_arm(){
 	var mid = (0.5 + arm.position) * station_width;
@@ -76,10 +87,10 @@ function draw_line(f_horizontal_start, f_vertical_start, f_horizontal_end, f_ver
 function draw_rectangle(f_horizontal_start, f_vertical_start, f_width, f_height){
 	var frame = document.getElementById("frame");
 	var ctx = frame.getContext("2d");
-	ctx.lineWidth=1;
+	ctx.lineWidth=4;
 	ctx.rect(f_horizontal_start,f_vertical_start,f_width,f_height);
 	ctx.strokeStyle = "#000000";
-  ctx.stroke();
+  	ctx.stroke();
 
 	ctx.fillStyle = brush;
 	ctx.fill();
@@ -93,75 +104,148 @@ function draw_assembly_line(){
 		var left = i * station_width;
 		var right = left + station_width;
 
-		  draw_line(left, line_position, right, line_position);
+		draw_line(left, line_position, right, line_position);
    		draw_line(left, line_position - 5, left, line_position);
     	draw_line(right, line_position - 5, right, line_position);
 
-      robot_arm.assembly_line[i] = levels["exercise_9"][i];
+      	robot_arm.assembly_line[i] = levels["exercise_" + exercise][i];
     	// stack is the current table of blocks 
     	var stack = robot_arm.assembly_line[i];
     	if(stack != null){
-        for (var level = 0; level <= stack.length -1  ; level++) {
-          if(robot_arm.assembly_line[i][level] != null){
-            console.log(robot_arm.assembly_line[i][level]);
-            var current_color = 'white';
+	        for (var level = 0; level <= stack.length -1  ; level++) {
+	          if(robot_arm.assembly_line[i][level] != null){
+	            console.log(robot_arm.assembly_line[i][level]);
+	            var current_color = 'white';
 
-            if(robot_arm.assembly_line[i][level] == "r" || robot_arm.assembly_line[i][level] =="red"){
-              current_color = "red";
-            } 
+	            if(robot_arm.assembly_line[i][level] == "r" || robot_arm.assembly_line[i][level] =="red"){
+	              current_color = "red";
+	            } 
 
-            if(robot_arm.assembly_line[i][level] == "g" || robot_arm.assembly_line[i][level] =="green"){
-              current_color = "green";
-            } 
+	            if(robot_arm.assembly_line[i][level] == "g" || robot_arm.assembly_line[i][level] =="green"){
+	              current_color = "green";
+	            } 
 
-            if(robot_arm.assembly_line[i][level] == "b" || robot_arm.assembly_line[i][level] =="blue"){
-              current_color = "blue";
-            }
+	            if(robot_arm.assembly_line[i][level] == "b" || robot_arm.assembly_line[i][level] =="blue"){
+	              current_color = "blue";
+	            }
 
-            brush = current_color;
-          }
-          draw_rectangle(left + 5, line_position - block_height * (level + 1) - (2 * (level + 1)) , block_width, block_height);
-        }
+	            brush = current_color;
+	          }
+	          draw_rectangle(left + 5, line_position - block_height * (level + 1) - (4 * (level + 1)) , block_width, block_height);
+	        }
     		
     	}
 
 
 	}
 }
-/*
-local function draw_assembly_line(dc)
-  for i = 1, station_count do
-    local left = (i - 1) * station_width
-    local right = left + station_width
-    
-    dc:DrawLine(left, line_position, right, line_position)
-    dc:DrawLine(left, line_position - 5, left, line_position)
-    dc:DrawLine(right, line_position - 5, right, line_position)
-    
-    local stack = robot_arm.assembly_line[i]
-    if type(stack) == 'table' then
-      for level, block in ipairs(stack) do
-        local color = wx.wxWHITE_BRUSH
-        
-        if block == 'r' or block == 'red' then
-          color = wx.wxRED_BRUSH
-        elseif block == 'g' or block == 'green' then
-          color = wx.wxGREEN_BRUSH
-        elseif block == 'b' or block == 'blue' then
-          color = wx.wxBLUE_BRUSH
-        end
-        
-        dc:SetBrush(color)
-        dc:DrawRectangle(left + 5, line_position - block_height * level, block_width, block_height)
-      end
-    end
-  end
-end
 
-*/
+
+function paint(){
+	var frame = document.getElementById("frame");
+	var ctx = frame.getContext("2d");
+	ctx.clearRect(0, 0, frame.width, frame.height);
+	draw_arm();
+ 	draw_assembly_line();
+}
+
+
+function animate(start_value, end_value, duration){
+	var actual_duration = duration * 0.5;
+
+	if(typeof(robot_arm.speed) == "number"){
+		actual_duration = duration * (1 - Math.floor(robot_arm.speed));
+	}
+
+	if(actual_duration < 1){
+		actual_duration = 0;
+	}
+
+	// fixing corutine
+	var diff = end_value - start_value;
+	var counter = 0;
+	var stop_watch = setInterval(function(){
+		var fraction = Math.min(1, counter / actual_duration);
+		while(fraction < 1){
+			fraction = Math.min(1, counter / actual_duration);
+			counter = counter + framerate;
+		}
+		clearInterval(stop_watch);
+		
+	}, framerate);
+
+}
+
+function refresh_arm(){
+	var left = arm.position * station_width;
+	var width = station_width;
+	var top = 0;
+	var height = arm_height + arm.level * block_height + hand_height;
+
+	if(arm.holding != null){
+		height = height + block_height;
+	}
+	paint();
+}
+
+function increase_actions(){
+	arm.actions = arm.actions + 1;
+}
+
+function animate_arm(property_name, start_value, end_value, duration){
+	if(robot_arm.speed >= 1){
+    	arm[property_name] = end_value
+    	return;
+	}
+
+	var value = animate(start_value, end_value, duration);
+    refresh_arm();
+  
+}
+
+function move_right(){
+	if(arm.position >= station_count - 1){
+		return;
+	}
+	animate_arm('position', arm.position, arm.position += 1, max_duration);
+	increase_actions();
+}
+
+function move_left(){
+	if(arm.position <= 0){
+		return;
+	}
+	animate_arm('position', arm.position, arm.position -= 1, max_duration);
+	increase_actions();
+}
+
+function grab(){
+	var stack = robot_arm.assembly_line[arm.position + 1];
+	var grab_level = level_count - stack.length;
+
+	if(stack.length == 0){
+		grab_level = grab_level - 1;
+	}
+
+	if (arm.holding != null){
+		grab_level = grab_level - 1;
+	}
+
+	animate_arm('level', 0, grab_level, max_duration);
+
+	if( arm.holding == null ){
+		arm.holding = stack[stack.length - 2];
+		stack[stack.length - 2] = null;
+	}
+
+	animate_arm('level', grab_level, 0, max_duration);
+  	increase_actions();
+
+}
+
 
 jQuery(document).ready(function(){
- arm.holding = "r";
+ set_level(9);
  draw_arm();
  draw_assembly_line();
 
