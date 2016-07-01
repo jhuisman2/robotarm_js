86e79aab172d2a3a31b02e7a5729c838e8958ddb
Jan-Willem Huisman <jhuisman2@gmail.com>
2016-07-01 10:23:09 +0200

    Fixed level loading

--- robotarm.js
+++ robotarm.js
@@ -8,8 +8,8 @@ var arm = {
 }
 
 var robot_arm = {
-  speed = 0.5,
-  assembly_line = {}
+  speed: 0.5,
+  assembly_line : {}
 }
 
 var max_duration = 2000;
@@ -52,7 +52,7 @@ function draw_arm(){
   		}
 
   		brush = current_color;
-  		draw_rectangle(left + 1, top + 1, block_width, block_height);
+  		//draw_rectangle(left + 1, top + 1, block_width, block_height);
   	}
 }
 
@@ -78,7 +78,9 @@ function draw_rectangle(f_horizontal_start, f_vertical_start, f_width, f_height)
 	var ctx = frame.getContext("2d");
 	ctx.lineWidth=1;
 	ctx.rect(f_horizontal_start,f_vertical_start,f_width,f_height);
-	ctx.stroke();
+	ctx.strokeStyle = "#000000";
+  ctx.stroke();
+
 	ctx.fillStyle = brush;
 	ctx.fill();
 }
@@ -91,13 +93,35 @@ function draw_assembly_line(){
 		var left = i * station_width;
 		var right = left + station_width;
 
-		draw_line(left, line_position, right, line_position);
+		  draw_line(left, line_position, right, line_position);
    		draw_line(left, line_position - 5, left, line_position);
     	draw_line(right, line_position - 5, right, line_position);
 
+      robot_arm.assembly_line[i] = levels["exercise_9"][i];
     	// stack is the current table of blocks 
     	var stack = robot_arm.assembly_line[i];
     	if(stack != null){
+        for (var level = 0; level <= stack.length -1  ; level++) {
+          if(robot_arm.assembly_line[i][level] != null){
+            console.log(robot_arm.assembly_line[i][level]);
+            var current_color = 'white';
+
+            if(robot_arm.assembly_line[i][level] == "r" || robot_arm.assembly_line[i][level] =="red"){
+              current_color = "red";
+            } 
+
+            if(robot_arm.assembly_line[i][level] == "g" || robot_arm.assembly_line[i][level] =="green"){
+              current_color = "green";
+            } 
+
+            if(robot_arm.assembly_line[i][level] == "b" || robot_arm.assembly_line[i][level] =="blue"){
+              current_color = "blue";
+            }
+
+            brush = current_color;
+          }
+          draw_rectangle(left + 5, line_position - block_height * (level + 1) - (2 * (level + 1)) , block_width, block_height);
+        }
     		
     	}
 
@@ -142,3 +166,14 @@ jQuery(document).ready(function(){
  draw_assembly_line();
 
 });
+
+
+/* Levels */
+
+var levels = {
+  'exercise_1' : [ [], [ 'red']  ],
+  'exercise_6' : [ [ 'red' ], [ 'blue' ], [ 'white' ], [ 'green' ], [ 'green' ], ['blue' ], [ 'red' ], [ 'white' ] ] ,
+  'exercise_9' : [ ['blue' ], [ 'green', 'green' ], ['white', 'white', 'white' ], [ 'red', 'red', 'red', 'red' ] ],
+
+
+}
